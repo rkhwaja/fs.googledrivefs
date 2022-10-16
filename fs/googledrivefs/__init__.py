0d2c50fa7132c6f4c7501e3f3690145e7d44d251
Rehan Khwaja <rehan@khwaja.name>
2022-10-15 19:02:48 -0700

    Add null handler
    https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library

--- fs/googledrivefs/__init__.py
+++ fs/googledrivefs/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import
 
+from logging import getLogger, NullHandler
+
 from .googledrivefs import GoogleDriveFS, SubGoogleDriveFS
 from .opener import GoogleDriveFSOpener
 from .search import And, MimeTypeEquals, NameEquals
+
+getLogger(__name__).addHandler(NullHandler())
